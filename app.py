c5189581be3a6007854c6d90e1b7ec5e9a1fbef3
Vaghesh Patel <38082759+Patelvaghesh2313@users.noreply.github.com>
2020-09-14 16:11:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, g, session
 from passlib.hash import sha256_crypt
 from models import *
-
 
 app = Flask(__name__)
 app.secret_key = 'charitySystem'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:2313@localhost/CharitySystem'
 db.init_app(app)
-
 
 
 @app.route('/')
@@ -19,7 +16,9 @@ def index():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template("layout01.html")
+    return render_template("layout01.html",user=session['user'])
+
+
 # ----------LOGIN------------#
 
 
@@ -28,17 +27,20 @@ def login():
     if request.method == "GET":
         return render_template("login.html")
     else:
+        session.pop('user', None)
         username = request.form.get("username")
         password = request.form.get("password")
         myUser = User.query.filter_by(email=username).first()
-        if myUser is None:
-            print("NO USER")
+        if username == 'admin' and password == 'admin@123':
+            session['user'] = 'admin'
+            return redirect(url_for('admin'))
         else:
             if sha256_crypt.verify(password, myUser.password):
-                return redirect(url_for('admin'))
-            else:
-                print("Danger")
-        print("danger")
+                session['user'] = username
+                return redirect(url_for('dashboard'))
+            return redirect(url_for('login'))
+        return render_template("error.html")
+
 
 # ----------SIGIN-IN------------#
 
@@ -71,17 +73,30 @@ def sign_in():
 
 @app.route('/charity_details')
 def charity_details():
-    return render_template("charity_details.html")
+    if g.user:
+        return render_template("charity_details.html")
+    return render_template("error.html")
 
 
 @app.route('/past_transactions')
 def past_transactions():
-    return render_template("past_transactions.html")
+    if g.user:
+        return render_template("past_transactions.html")
+    return render_template("error.html")
 
 
 @app.route('/contact_us')
 def contact_us():
-    return render_template("contact_us.html")
+    if g.user:
+        return render_template("contact_us.html")
+    return render_template("error.html")
+
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return render_template("layout0.html")
 
 
 # ----------ADMIN-PART------------#
@@ -89,12 +104,23 @@ def contact_us():
 
 @app.route('/admin')
 def admin():
-    return render_template("admin_layout.html")
+    if session['user'] == 'admin':
+        return render_template("admin_layout.html", user=session['user'])
+    return render_template("error.html")
 
 
 @app.route('/user_details')
 def user_details():
-    return render_template("admin_user_details.html")
+    if session['user'] == 'admin':
+        return render_template("admin_user_details.html")
+    return render_template("error.html")
+
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
 
 
 if __name__ == '__main__':
