e011da4d0bc1e38f3027a37c05cc56756159dd48
Vaghesh Patel <38082759+Patelvaghesh2313@users.noreply.github.com>
2020-10-06 15:52:58 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -12,8 +13,20 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     __tablename__ = 'userDetails'
-    id = db.Column(db.Integer, primary_key=True , autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     fullname = db.Column(db.String, nullable=False)
     address = db.Column(db.String, nullable=False)
+    city = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
+    phone = db.Column(db.String,nullable=False)
     password = db.Column(db.String, nullable=False)
+
+
+class Charity(db.Model):
+    __tablename__ = 'charityDetails'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(120),default='image.jpg')
+    pub_time = db.Column(db.Date, nullable=False,default=datetime.utcnow)
+
